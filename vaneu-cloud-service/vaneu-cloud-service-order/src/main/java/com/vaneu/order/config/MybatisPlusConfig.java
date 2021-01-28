e50f48f5f80cb186c8a9bd9c509b88fa8ac79ef8
vaneu <403652287@qq.com>
2021-01-28 15:06:02 +0800

    1

--- vaneu-cloud-service/vaneu-cloud-service-order/src/main/java/com/vaneu/order/config/MybatisPlusConfig.java
+++ vaneu-cloud-service/vaneu-cloud-service-order/src/main/java/com/vaneu/order/config/MybatisPlusConfig.java
@@ -4,8 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 
 /**
  * @ClassName: MybatisPlusConfig
@@ -17,19 +19,36 @@ import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 @MapperScan("com.vaneu.order.mapper")
 public class MybatisPlusConfig {
 
-	/** 分页插件 */
-	@Bean
-	public PaginationInterceptor paginationInterceptor() {
-		return new PaginationInterceptor();
-	}
+//	/** 分页插件 */
+//	@Bean
+//	public PaginationInterceptor paginationInterceptor() {
+//		return new PaginationInterceptor();
+//	}
+//
+//	/**
+//	 * @Title: optimisticLockerInterceptor
+//	 * @Description: TODO(乐观锁)
+//	 * @return OptimisticLockerInterceptor
+//	 */
+//	@Bean
+//	public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+//		return new OptimisticLockerInterceptor();
+//	}
 
 	/**
-	 * @Title: optimisticLockerInterceptor
-	 * @Description: TODO(乐观锁)
-	 * @return OptimisticLockerInterceptor
+	 * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor
+	 * = false 避免缓存出现问题
 	 */
 	@Bean
-	public OptimisticLockerInterceptor optimisticLockerInterceptor() {
-		return new OptimisticLockerInterceptor();
+	public MybatisPlusInterceptor mybatisPlusInterceptor() {
+		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+		interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+		return interceptor;
+	}
+
+	@SuppressWarnings("deprecation")
+	@Bean
+	public ConfigurationCustomizer configurationCustomizer() {
+		return configuration -> configuration.setUseDeprecatedExecutor(false);
 	}
 }
